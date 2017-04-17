82a5fd5cd78fa3db932fc88e5f58918871dfe497
isma3l <karlo.ismael@gmail.com>
2017-04-17 03:59:57 -0300

    se agrega el pipe

---
+++ src/core/pipe.h
@@ -0,0 +1,29 @@
+#ifndef PIPE_H_
+#define PIPE_H_
+
+
+class Pipe {
+	public:
+		Pipe();
+		void setModo(const int mode);
+		ssize_t leer(void* buffer, const int size);
+		ssize_t escribir(const void* buffer, const int size);
+		int getFdLectura() const;
+		int getFdEscritura() const;
+		void cerrarFdLectura();
+		void cerrarFdEscritura();
+		void cerrar();
+		~Pipe();
+
+		static const int MODO_LECTURA = 0;
+		static const int MODO_ESCRITURA = 1;
+
+	private:
+		int descriptores[2];
+		bool escritura;
+		bool lectura;
+		static const int CODE_ERROR = -1;
+
+};
+
+#endif /* PIPE_H_ */
